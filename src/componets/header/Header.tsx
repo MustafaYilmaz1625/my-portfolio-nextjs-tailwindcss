b1469648f8efb666bdf4ae4d88a081e870b7b809
Mustafa Yılmaz <105638480+MustafaYilmaz1625@users.noreply.github.com>
2023-01-29 01:00:39 +0300

    add react-router-dom on page

--- src/componets/header/Header.tsx
+++ src/componets/header/Header.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import Medicine from "../../assets/images/Medicine.svg"
+import {AiOutlineSearch} from "react-icons/ai"
 
 export default function Header() {
   return (
+    
     <div>Header</div>
   )
 }
