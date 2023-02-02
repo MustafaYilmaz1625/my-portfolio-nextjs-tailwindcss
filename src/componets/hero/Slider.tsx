6cdebe66648663b5ff02988e75ec00deb7472637
Mustafa Yılmaz <105638480+MustafaYilmaz1625@users.noreply.github.com>
2023-02-03 01:25:19 +0300

    add slinder .tsx css update on page

--- src/componets/hero/Slider.tsx
+++ src/componets/hero/Slider.tsx
@@ -5,8 +5,8 @@ import { slide } from "../../assets/data/data";
 export const Slider = () => {
   return (
     <>
-      <div className="slider">
-        <div className="container grid">
+      <div className="slider ">
+        <div className="max-w-[95px] m-auto grid gap-5 md:grid-cols-4 grid-cols-2">
           {slide.map((item, i) => (
             <div className="box" key={i}>
               <div className="img">
