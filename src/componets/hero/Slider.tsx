58de3d193c605100f7ea0e64ed1ec38d8ca8cb94
Mustafa Yılmaz <105638480+MustafaYilmaz1625@users.noreply.github.com>
2023-02-03 01:06:20 +0300

    add slider.tsx on page

---
+++ src/componets/hero/Slider.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./slider.css";
+import { slide } from "../../assets/data/data";
+
+export const Slider = () => {
+  return (
+    <>
+      <div>silider</div>
+    </>
+  );
+};
