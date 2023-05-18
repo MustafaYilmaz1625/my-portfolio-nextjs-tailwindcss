27ab537b4bb10563f245c453eced9463ead3dcf9
Mustafa Yılmaz <105638480+MustafaYilmaz1625@users.noreply.github.com>
2023-05-18 23:44:22 +0300

    create icons

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,19 +1,37 @@
 import React from 'react'
 import Link from "next/link"
 import Logo from './Logo';
+import { useRouter } from 'next/router';
+import TwitterIcon from './Icons';
+
+
+const CustomLink = ({href,title,className=""}) => {
+    const router = useRouter()
+    return (
+      <Link href={href} className={`${className} relative group`}>
+        {title}
+        <span
+          className={`h-[1.5px] inline-block bg-black absolute left-0 bottom-0 group-hover:w-full transition-[width] ease duration-300 ${router.asPath === href ? 'w-full' : 'w-0'}`}
+        >
+          {" "}
+          &nbsp;
+        </span>
+      </Link>
+    );
+}
+
 const NavBar = () => {
   return (
     <header className="w-full px-32 py-8 font-medium flex items-center justify-between">
       <nav>
-        <Link href="/">Home</Link>
-        <Link href="/about">About</Link>
-        <Link href="/project">Project</Link>
-        <Link href="/articles">Articles</Link>
+        <CustomLink href="/" title="Home" className="mr-4" />
+        <CustomLink href="/about" title="About" className="mx-4" />
+        <CustomLink href="/project" title="Project" className="mx-4" />
+        <CustomLink href="/articles" title="Articles" className="ml-4" />
       </nav>
-      <Logo/>
       <nav>
         <Link href="/" target={"_blank"}>
-          T
+          <TwitterIcon/>
         </Link>
         <Link href="/" target={"_blank"}>
           T
@@ -31,6 +49,9 @@ const NavBar = () => {
           T
         </Link>
       </nav>
+      <div className="absolute left-[50%] top-2 translate-x-[-50%]">
+        <Logo />
+      </div>
     </header>
   );
 }
