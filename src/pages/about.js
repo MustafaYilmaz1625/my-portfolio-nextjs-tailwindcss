b99d91acca77d18aeee70a7b231d09abb39fcf9a
Mustafa Yılmaz <105638480+MustafaYilmaz1625@users.noreply.github.com>
2023-05-23 17:48:31 +0300

    create AnimatedNumbers

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,29 +6,29 @@ import Image from "next/image";
 import profilePic from "../../public/images/profile/developer-pic-2.jpg";
 import { useInView, useMotionValue, useSpring } from "framer-motion";
 
-// const AnimatedNumbers = ({ value }) => {
-//   const ref = useRef(null);
+const AnimatedNumbers = ({ value }) => {
+  const ref = useRef(null);
 
-//   const motionValue = useMotionValue(0);
-//   const springValue = useSpring(motionValue, { duration: 3000 });
-//   const isInView = useInView(ref);
+  const motionValue = useMotionValue(0);
+  const springValue = useSpring(motionValue, { duration: 3000 });
+  const isInView = useInView(ref,{once:true});
 
-//   useEffect(() => {
-//     if (isInView) {
-//       motionValue.set(value);
-//     }
-//   }, [isInView, value, motionValue]);
+  useEffect(() => {
+    if (isInView) {
+      motionValue.set(value);
+    }
+  }, [isInView, value, motionValue]);
 
-//   useEffect(() => {
-//     springValue.on("change", (latest) => {
-//       if (ref.current && latest.toFixed(0) <= value) {
-//         ref.current.textContent = latest.toFixed(0);
-//       }
-//     });
-//   }, [springValue, value]);
+  useEffect(() => {
+    springValue.on("change", (latest) => {
+      if (ref.current && latest.toFixed(0) <= value) {
+        ref.current.textContent = latest.toFixed(0);
+      }
+    });
+  }, [springValue, value]);
 
-//   return <span ref={ref}></span>;
-// };
+  return <span ref={ref}></span>;
+};
 
 const about = () => {
   return (
