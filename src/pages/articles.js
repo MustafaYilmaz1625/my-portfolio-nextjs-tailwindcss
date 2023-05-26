e7786d92c77493064946587a4060a852e0adfd45
Mustafa Yılmaz <105638480+MustafaYilmaz1625@users.noreply.github.com>
2023-05-26 13:49:45 +0300

    article list CSS has been improved

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -3,25 +3,69 @@ import Layout from "@/components/Layout";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useRef } from "react";
 import article1 from "../../public/images/articles/pagination component in reactjs.jpg";
 import article2 from "../../public/images/articles/create loading screen in react js.jpg";
-import { motion } from "framer-motion";
-import article3 from "../../public/images/articles/create modal component in react using react portals.png"
+import { motion, useMotionValue } from "framer-motion";
+import article3 from "../../public/images/articles/create modal component in react using react portals.png";
+import article4 from "../../public/images/articles/form validation in reactjs using custom react hook.png";
+import article5 from "../../public/images/articles/smooth scrolling in reactjs.png";
 
 const FramerImage = motion(Image);
 
-const Article =({img,title,data,link})=>{
-    return(
-        <li className="relative w-full p-4 py-6 my-4 rounded-xl flex items-center justify-between bg-light text-dark first:mt-0 border border-solid border-dark
-        border-r-4 border-b-4 ">
-            <Link href={link} target="_blank">
-            <h2 className="capitalizetext-xl font-semibold hover:underline">{title}</h2>
-            </Link>
-            <span className="text-primary font-semibold pl-4">{data} </span>
-        </li>
-    )
-}
+const MovingImg = ({ img, title, link }) => {
+  const x = useMotionValue(0);
+  const y = useMotionValue(0);
+  const imgRef = useRef(null);
+
+  function handleMouse(event) {
+    imgRef.current.style.display = "inline-block";
+    x.set(event.pageX);
+    y.set(-10);
+  }
+  function handleMouseLeave(event) {
+    imgRef.current.style.display = "none";
+    x.set(0);
+    y.set(0);
+  }
+
+  return (
+    <Link
+      href={link}
+      target="_blank"
+      onMouseMove={handleMouse}
+      onMouseLeave={handleMouseLeave}
+    >
+      <h2 className="capitalize text-xl font-semibold hover:underline">
+        {title}
+      </h2>
+      <FramerImage
+        style={{ x: x, y: y }}
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1, transition: { duration: 0.2 } }}
+        ref={imgRef}
+        src={img}
+        alt={title}
+        className="z-10 w-96 h-auto hidden absolute rounded-lg "
+      />
+    </Link>
+  );
+};
+
+const Article = ({ img, title, data, link }) => {
+  return (
+    <motion.li
+      initial={{ y:200 }}
+      whileInView={{ y:0, transition: { duration: 0.5, ease:"easeInOut" } }}
+      viewport={{once:true}}
+      className="relative w-full p-4 py-6 my-4 rounded-xl flex items-center justify-between bg-light text-dark first:mt-0 border border-solid border-dark
+        border-r-4 border-b-4 "
+    >
+      <MovingImg title={title} img={img} link={link} />
+      <span className="text-primary font-semibold pl-4">{data} </span>
+    </motion.li>
+  );
+};
 
 const FeaturedArticle = ({ title, img, time, summary, link }) => {
   return (
@@ -94,13 +138,13 @@ Follow this step-by-step guide to integrate Pagination component in your ReactJS
               title="Form Validation In Reactjs: Build A Reusable Custom Hook For Inputs And Error Handling"
               data="March 22,2023"
               link="/"
-              img={article3}
+              img={article4}
             />
             <Article
               title="Form Validation In Reactjs: Build A Reusable Custom Hook For Inputs And Error Handling"
               data="March 22,2023"
               link="/"
-              img={article3}
+              img={article5}
             />
           </ul>
         </Layout>
