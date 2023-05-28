00a0187cc51584ff657714adcef9b8b7dfb66d9f
Mustafa Yılmaz <105638480+MustafaYilmaz1625@users.noreply.github.com>
2023-05-28 16:52:18 +0300

    home apply responsive on js page

--- src/components/AnimatedText.js
+++ src/components/AnimatedText.js
@@ -31,6 +31,7 @@ const AnimatedText = ({text,className=''}) => {
   return (
     <div
       className="w-full mx-auto py-2 flex items-center justify-center text-center overflow-hidden
+      sm:py-0
     "
     >
       <motion.h1
